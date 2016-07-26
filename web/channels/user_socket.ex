f8676981a308e2c81aa8c7526795fcfe261bb073
Nick Doiron <ndoiron@mapmeld.com>
2016-07-25 22:55:15 -0400

    heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Superfundme.UserSocket do
   # channel "rooms:*", Superfundme.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
