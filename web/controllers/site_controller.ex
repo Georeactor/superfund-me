150115ae4c6fb6fa709fe09972674da94f819d6d
Nick Doiron <ndoiron@mapmeld.com>
2016-09-09 16:41:29 -0400

    include static files for map

--- web/controllers/site_controller.ex
+++ web/controllers/site_controller.ex
@@ -10,21 +10,23 @@ defmodule Superfundme.SiteController do
     render(conn, "index.html", sites: sites)
   end
 
-  def new(conn, _params) do
+  def new(conn, params) do
     changeset = Site.changeset(%Site{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"site" => site_params}) do
-    changeset = Site.changeset(%Site{}, site_params)
+  def create(conn, %{"key" => key, "site" => site_params}) do
+    if key == System.get_env("SECRET_QUERY") do
+      changeset = Site.changeset(%Site{}, site_params)
 
-    case Repo.insert(changeset) do
-      {:ok, _site} ->
-        conn
-        |> put_flash(:info, "Site created successfully.")
-        |> redirect(to: site_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+      case Repo.insert(changeset) do
+        {:ok, _site} ->
+          conn
+          |> put_flash(:info, "Site created successfully.")
+          |> redirect(to: site_path(conn, :index))
+        {:error, changeset} ->
+          render(conn, "new.html", changeset: changeset)
+      end
     end
   end
 
@@ -39,29 +41,33 @@ defmodule Superfundme.SiteController do
     render(conn, "edit.html", site: site, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "site" => site_params}) do
-    site = Repo.get!(Site, id)
-    changeset = Site.changeset(site, site_params)
+  def update(conn, %{"id" => id, "key" => key, "site" => site_params}) do
+    if key == System.get_env("SECRET_QUERY") do
+      site = Repo.get!(Site, id)
+      changeset = Site.changeset(site, site_params)
 
-    case Repo.update(changeset) do
-      {:ok, site} ->
-        conn
-        |> put_flash(:info, "Site updated successfully.")
-        |> redirect(to: site_path(conn, :show, site))
-      {:error, changeset} ->
-        render(conn, "edit.html", site: site, changeset: changeset)
+      case Repo.update(changeset) do
+        {:ok, site} ->
+          conn
+          |> put_flash(:info, "Site updated successfully.")
+          |> redirect(to: site_path(conn, :show, site))
+        {:error, changeset} ->
+          render(conn, "edit.html", site: site, changeset: changeset)
+      end
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    site = Repo.get!(Site, id)
+  def delete(conn, %{"id" => id, "key" => key}) do
+    if key == System.get_env("SECRET_QUERY") do
+      site = Repo.get!(Site, id)
 
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(site)
+      # Here we use delete! (with a bang) because we expect
+      # it to always work (and if it does not, it will raise).
+      Repo.delete!(site)
 
-    conn
-    |> put_flash(:info, "Site deleted successfully.")
-    |> redirect(to: site_path(conn, :index))
+      conn
+      |> put_flash(:info, "Site deleted successfully.")
+      |> redirect(to: site_path(conn, :index))
+    end
   end
 end
